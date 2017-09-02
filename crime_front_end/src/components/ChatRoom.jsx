8b5e32a9f5ef8d532972d1db6c4c452833fcdb3e
g k <gurbraj.kang@gmail.com>
2017-09-01 20:13:27 -0700

    done setup socket on server and client

--- crime_front_end/src/components/ChatRoom.jsx
+++ crime_front_end/src/components/ChatRoom.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import openSocket from 'socket.io-client';
+const url = "http://localhost:4000"
+const socket = openSocket(url)
 
 
 class ChatRoom extends React.Component {
+  componentDidMount() {
+    let that = this;
+    socket.on('chat message', function (messageObj) {
+      console.log(messageObj)
+    })
+    socket.emit('chat message', "message from client");
+  }
   render() {
     return (
       <div>
         this is chatroom {this.props.messages}
         </div>
-    )    
+    )
   }
 }
 
